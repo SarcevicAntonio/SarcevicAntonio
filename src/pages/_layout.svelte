e574051bbd24e07c7df80e2eee8166aa1ac461be
Antonio Sarcevic <sarcevicantonio@gmailcom>
2021-02-03 21:17:10 +0100

    copied routify starter

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,20 @@
+<header>random header content</header>
+<main>
+  <!-- routify:options preload="proximity" -->
+  <slot />
+</main>
+
+<style type="scss">
+  header {
+    border: 1px solid black;
+    border-radius: 8px;
+    padding: 8px;
+    text-align: center;
+  }
+  main {
+    border: 1px solid black;
+    border-radius: 8px;
+    margin-top: 8px;
+    padding: 8px;
+  }
+</style>
---
+++ src/pages/index.svelte
@@ -0,0 +1,31 @@
+<script type="ts">
+  import { metatags, url } from "@roxi/routify";
+  metatags.title = "Hello Worldtify";
+  metatags.description = "Description coming soon...";
+
+  const msg: string = "World";
+  let value: string = "a-random-page";
+</script>
+
+<h2>Hello {msg}!</h2>
+<div>
+  <label>
+    visit a page:
+    <input type="text" bind:value />
+    <a href={$url('/id/'+value)}>
+      <button>go there</button>
+    </a>
+  </label>
+</div>
+
+<style type="scss">
+  label {
+    font-size: 16px;
+    input {
+      font-size: 16px;
+    }
+    button {
+      font-size: 16px;
+    }
+  }
+</style>

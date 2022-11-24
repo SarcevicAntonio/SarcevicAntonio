381559390b5106d8d70c74fe876c4e42728fa650
Antonio Sarcevic <antonio.sarcevic@web-computing.de>
2022-11-24 18:13:10 +0100

    add my themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,38 @@ export default {
   plugins: [typography, daisyui],
   daisyui: {
     themes: [
+      {
+        'as-light': {
+          primary: '#a3e635',
+          secondary: '#c084fc',
+          accent: '#1FB2A6',
+          neutral: '#111827',
+          'base-100': '#dcfce7'
+        }
+      },
+      {
+        'as-dark': {
+          primary: '#bef264',
+          secondary: '#c084fc',
+          accent: '#1FB2A6',
+          neutral: '#111827',
+          'base-100': '#1e0d33'
+        }
+      },
+      {
+        'original-light': {
+          primary: 'hsl(161, 50%, 42%)',
+          secondary: 'hsl(281, 50%, 42%)',
+          'base-100': 'hsl(161, 100%, 97%)'
+        }
+      },
+      {
+        'original-dark': {
+          primary: 'hsl(161, 50%, 58%)',
+          secondary: 'hsl(281, 50%, 58%)',
+          'base-100': 'hsl(281, 100%, 6%)'
+        }
+      },
       'light',
       'dark',
       'cupcake',
@@ -40,6 +72,7 @@ export default {
       'night',
       'coffee',
       'winter'
-    ]
+    ],
+    darkTheme: 'as-dark'
   }
 }

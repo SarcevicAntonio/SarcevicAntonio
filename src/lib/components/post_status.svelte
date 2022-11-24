974ff1380904e22b8e7e6357ee5e4fd0626894aa
Antonio Sarcevic <antonio.sarcevic@web-computing.de>
2022-11-24 18:41:23 +0100

    hide dates if not published

--- src/lib/components/post_status.svelte
+++ src/lib/components/post_status.svelte
@@ -17,19 +17,21 @@
     href={site.protocol + site.domain}>
     {site.author.name}
   </a>
-  <span class:hidden={preview} class="opacity-50">/</span>
-  <a href={post.path} class="u-url u-uid swap group/time">
-    <time
-      class="group-hover/time:opacity-0 font-semibold opacity-75 duration-500 ease-in-out mr-auto dt-published"
-      datetime={jsonPublished}
-      itemprop="datePublished">
-      {stringPublished}
-    </time>
-    <time
-      class="opacity-0 group-hover/time:opacity-100 font-semibold text-primary duration-500 ease-in-out mr-auto dt-updated"
-      datetime={jsonUpdated}
-      itemprop="dateModified">
-      {stringUpdated}
-    </time>
-  </a>
+  {#if post.published}
+    <span class:hidden={preview} class="opacity-50">/</span>
+    <a href={post.path} class="u-url u-uid swap group/time">
+      <time
+        class="group-hover/time:opacity-0 font-semibold opacity-75 duration-500 ease-in-out mr-auto dt-published"
+        datetime={jsonPublished}
+        itemprop="datePublished">
+        {stringPublished}
+      </time>
+      <time
+        class="opacity-0 group-hover/time:opacity-100 font-semibold text-primary duration-500 ease-in-out mr-auto dt-updated"
+        datetime={jsonUpdated}
+        itemprop="dateModified">
+        {stringUpdated}
+      </time>
+    </a>
+  {/if}
 </div>

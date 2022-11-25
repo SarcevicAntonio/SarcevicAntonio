42481454c3540423f1f808884d4d12d03276fb22
Antonio Sarcevic <antonio.sarcevic@web-computing.de>
2022-11-25 17:17:42 +0100

    lets try utterances

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -2,17 +2,9 @@ import type { PostConfig } from '$lib/types/post'
 
 export const post: PostConfig = {
   comment: {
-    use: ['Giscus'],
-    giscus: {
-      repo: 'SarcevicAntonio/SarcevicAntonio',
-      repoID: 'MDEwOlJlcG9zaXRvcnkxODkzMDAxNjM=',
-      category: 'Comments',
-      categoryID: 'DIC_kwDOC0h9w84CSwfL',
-      reactionsEnabled: true,
-      inputPosition: 'top',
-      lang: 'en',
-      theme: 'preferred_color_scheme',
-      loading: 'lazy'
+    use: ['Utterances'],
+    utterances: {
+      repo: 'SarcevicAntonio/SarcevicAntonio'
     }
   }
 }

8c477089a02cb1f8c64f1eb3ce15f5fcffe972ab
Antonio Sarcevic <antonio.sarcevic@web-computing.de>
2022-11-25 17:09:04 +0100

    comments

--- src/lib/config/post.ts
+++ src/lib/config/post.ts
@@ -1,3 +1,18 @@
 import type { PostConfig } from '$lib/types/post'
 
-export const post: PostConfig = {}
+export const post: PostConfig = {
+  comment: {
+    use: ['Giscus'],
+    giscus: {
+      repo: 'SarcevicAntonio/SarcevicAntonio',
+      repoID: 'MDEwOlJlcG9zaXRvcnkxODkzMDAxNjM=',
+      category: 'Comments',
+      categoryID: 'DIC_kwDOC0h9w84CSwfL',
+      reactionsEnabled: true,
+      inputPosition: 'top',
+      lang: 'en',
+      theme: 'preferred_color_scheme',
+      loading: 'lazy'
+    }
+  }
+}

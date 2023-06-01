52a96852eb28bf239d70bfcd38a23f017caef01a
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-01 13:51:19 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores'
+</script>
+
+<h1>{$page.status}</h1>
+
+{#if $page.status === 404}
+	<p>
+		Looks like {$page.url.href} was not fount...
+	</p>
+	<a href="/">Go back home!</a>
+{:else}
+	<p>{$page.error?.message}</p>
+{/if}
+
+<style>
+</style>

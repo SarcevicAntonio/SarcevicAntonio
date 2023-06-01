a71f6e3b4956b13497aed5d42fceaa912a40e496
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-01 14:25:33 +0200

    error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,16 +2,34 @@
 	import { page } from '$app/stores'
 </script>
 
-<h1>{$page.status}</h1>
+<div class="error">
+	<section>
+		<h1>{$page.status}</h1>
 
-{#if $page.status === 404}
-	<p>
-		Looks like {$page.url.href} was not fount...
-	</p>
-	<a href="/">Go back home!</a>
-{:else}
-	<p>{$page.error?.message}</p>
-{/if}
+		{#if $page.status === 404}
+			<p>
+				Looks like `{$page.url.pathname}` was not found...
+				<br />
+				Sorry about that 🫤
+			</p>
+			<p>
+				<a href="/">Go to `/` instead</a>
+			</p>
+		{:else}
+			<p>{$page.error?.message}</p>
+		{/if}
+	</section>
+</div>
 
 <style>
+	.error {
+		display: grid;
+		place-content: center;
+		position: relative;
+		min-height: 100dvh;
+	}
+
+	section {
+		max-width: 25rem;
+	}
 </style>

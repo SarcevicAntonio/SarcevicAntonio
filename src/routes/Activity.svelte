34e489dc9d746399915e6d940903d05ef9b2d04f
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-05 01:05:12 +0200

    tweak last.fm

--- src/routes/Activity.svelte
+++ src/routes/Activity.svelte
@@ -5,18 +5,32 @@
 </script>
 
 <h2>Music</h2>
-<h2>top tracks last 7 days</h2>
+<p>top scrobbeled tracks last 7 days</p>
 <ol>
 	{#each lastfm.toptracks.track as track}
+		{@const minutes = +track.duration * +track.playcount}
 		<li>
-			{track.artist.name} – {track.name}
+			<a href={track.url}>
+				{track.artist.name} – {track.name}
+				{#if +track.duration}
+					(~{minutes}
+					{minutes === 1 ? 'minute' : 'minutes'})
+				{/if}
+			</a>
 		</li>
 	{/each}
 </ol>
 
 <style>
-	h2,
+	h2 {
+		font-size: var(--step-5);
+		margin-block-end: 0.25em;
+	}
+
 	ol {
+		font-size: var(--step-0);
+		width: max-content;
 		margin: auto;
+		text-align: start;
 	}
 </style>

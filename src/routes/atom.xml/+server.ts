5f830aa354ed2a19753795f87ff2fd7139262883
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-04 12:26:40 +0200

    sitemap got

--- src/routes/atom.xml/+server.ts
+++ src/routes/atom.xml/+server.ts
@@ -1,4 +1,7 @@
+import domain from '$lib/domain'
 import { get_all_tags, get_blog_posts, type BlogMetadata } from '$lib/posts'
+
+export const prerender = true
 
 export async function GET() {
 	const content = await get_blog_posts(true)
@@ -14,40 +17,36 @@ export async function GET() {
 function get_feed(content: BlogMetadata[], all_tags: string[]): BodyInit | null | undefined {
 	return /*XML*/ `
 <feed xmlns="http://www.w3.org/2005/Atom">
-	<id>https://sarcevic.dev/</id>
-	<title><![CDATA[ Antonio Sarcevic ]]></title>
-	<subtitle> <![CDATA[ my personal blog ]]> </subtitle>
-	<icon>https://sarcevic.dev/favicon.png</icon>
-	<link href="https://sarcevic.dev" />
-	<link href="https://sarcevic.dev/atom.xml" rel="self" type="application/atom+xml" />
-	<updated>${new Date().toJSON()}</updated>
-	<author>
-		<name> <![CDATA[ Antonio Sarcevic ]]> </name>
-	</author>
+    <id>${domain}/</id>
+    <title><![CDATA[ Antonio Sarcevic ]]></title>
+    <subtitle> <![CDATA[ my personal blog ]]> </subtitle>
+    <icon>${domain}/favicon.png</icon>
+    <link href="${domain}" />
+    <link href="${domain}/atom.xml" rel="self" type="application/atom+xml" />
+    <updated>${new Date().toJSON()}</updated>
+    <author>
+        <name> <![CDATA[ Antonio Sarcevic ]]> </name>
+    </author>
 ${all_tags
-	.map(
-		(tag) => /*XML*/ `
-	<category term="${tag}" scheme="https://sarcevic.dev/?tags=${encodeURI(tag)}"/>`
-	)
-	.join('')}
+	.map((tag) => /*XML*/ `    <category term="${tag}" scheme="${domain}/?tags=${encodeURI(tag)}"/>`)
+	.join('\n')}
 ${content.map(
-	(post) => /*XML*/ `\n  <entry>
-	<title type="html"><![CDATA[${post.title}]]></title>
-	<link href="https://sarcevic.dev/${post.href}" />
-	<id>https://sarcevic.dev/${post.href}</id>
-	<published>${new Date(post.published).toJSON()}</published>
-	<updated>${new Date(post.updated ?? post.published).toJSON()}</updated>${
-		post.summary ? `\n    <summary type="html"><![CDATA[${post.summary}]]></summary>` : ''
-	}
-	<content type="html">
-	  <![CDATA[${post.html}]]>
-	</content>${post.tags
-		?.map(
-			(tag) =>
-				`\n    <category term="${tag}" scheme="http://sarcevic.dev/?tags=${encodeURI(tag)}" />`
-		)
-		.join('')}
- </entry>`
+	(post) => /*XML*/ `    <entry>
+        <title type="html"><![CDATA[${post.title}]]></title>
+        <link href="${domain}/${post.href}" />
+        <id>${domain}/${post.href}</id>
+        <published>${new Date(post.published).toJSON()}</published>
+        <updated>${new Date(post.updated ?? post.published).toJSON()}</updated>
+        ${post.summary ? /*XML*/ `<summary type="html"><![CDATA[${post.summary}]]></summary>` : ''}
+        <content type="html">
+            <![CDATA[${post.html}]]>
+        </content>
+    ${post.tags
+			?.map(
+				(tag) => /*XML*/ `<category term="${tag}" scheme="${domain}/?tags=${encodeURI(tag)}" />`
+			)
+			.join('')}
+    </entry>`
 )}
 </feed> `
 }
---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,35 @@
+import domain from '$lib/domain'
+import { get_blog_posts, type BlogMetadata } from '$lib/posts'
+import type { RequestHandler } from './$types'
+
+export const prerender = true
+
+export const GET: RequestHandler = async () => {
+	const content = await get_blog_posts(true)
+
+	return new Response(get_sitemap(content), {
+		headers: {
+			'content-type': 'application/xml; charset=utf-8',
+		},
+	})
+}
+
+// based of https://github.com/importantimport/urara
+function get_sitemap(content: BlogMetadata[]): string {
+	return /*XML*/ `<?xml version='1.0' encoding='utf-8'?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${domain}</loc>
+    </url>
+    ${content
+			.map(
+				(post) => `
+        <url>
+            <loc>${domain + '/' + post.href}</loc>
+            <lastmod>${new Date(post.updated ?? post.published).toISOString()}</lastmod>
+            <priority>0.5</priority>
+        </url>`
+			)
+			.join('')}
+</urlset>`
+}

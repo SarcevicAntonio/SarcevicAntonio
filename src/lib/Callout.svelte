751cc23765cc1d538bfb49f8edb063f899e8abdf
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-04 22:26:17 +0200

    fixup callout

--- src/lib/Callout.svelte
+++ src/lib/Callout.svelte
@@ -1,90 +1,52 @@
 <script lang="ts">
-	export let type: 'info' | 'warning' = 'info'
-	export let icon = ''
-	export let title = ''
- 
-	let defaultIcons = {
-	  info: 'i-material-symbols-info-outline-rounded',
-	  warning: 'i-material-symbols-warning-outline-rounded'
+	import type { ComponentType } from 'svelte'
+	import Info from '~icons/material-symbols/info-outline-rounded'
+	import Warning from '~icons/material-symbols/warning-outline-rounded'
+
+	let icons = {
+		info: Info,
+		warning: Warning,
 	}
- 
+
 	const capitalize = (string: string) => {
-	  return string ? string.charAt(0).toUpperCase() + string.slice(1) : ''
+		return string ? string.charAt(0).toUpperCase() + string.slice(1) : ''
 	}
- 
-	$: viewTitle = title || capitalize(type)
-	$: viewIcon = icon || defaultIcons[type]
- </script>
- 
- <section class={type}>
-	<div class="flex gap-2 items-center title">
-	  <div class={viewIcon} />
-	  <span class="font-bold">
-		 {viewTitle}
-	  </span>
-	</div>
+
+	export let type: 'info' | 'warning' = 'info'
+	export let icon: ComponentType = icons[type]
+	export let title = capitalize(type)
+</script>
+
+<section class={type}>
+	<p class="title">
+		<svelte:component this={icon} />
+		{title}
+	</p>
 	<div class="content">
-	  <slot />
+		<slot />
 	</div>
- </section>
- 
- <style>
+</section>
+
+<style>
 	section {
-	  border: 1px solid var(--color, var(--view-c));
-	  border-radius: 0.5rem;
+		border: 0.1em solid var(--as-text-1);
+
+		& :global(svg) {
+			vertical-align: sub;
+		}
+		padding: 1rem;
+		padding-block-end: 0.75rem;
+		border-radius: 1rem;
+		overflow: hidden;
 	}
- 
+
 	.title {
-	  background-color: var(--background-color, var(--view-bg));
-	  color: var(--color, var(--view-c));
- 
-	  padding: 1em;
-	  padding-bottom: 1.75em;
-	  border-top-left-radius: 0.5rem;
-	  border-top-right-radius: 0.5rem;
+		margin-block-start: -1rem;
+		margin-inline: -1rem;
+		margin-block-end: 0.5rem;
+		padding: 0.5rem 1rem;
+		font-size: 1.1em;
+		background-color: var(--as-text-1);
+		color: var(--as-back-1);
 	}
- 
-	:global(.prose) section :global(:where(strong):not(:where([class~='not-prose'] *))) {
-	  color: inherit;
-	}
- 
-	.content {
-	  border: 1px solid var(--color, var(--view-c));
- 
-	  background-color: hsl(var(--b1) / var(--tw-bg-opacity));
-	  padding: 1em;
-	  padding-top: 0.75em;
-	  margin: -1px;
-	  margin-top: -1em;
-	  border-radius: 0.5rem;
-	}
- 
-	.content > :global(:nth-child(1)) {
-	  margin-top: 0;
-	}
- 
-	.content > :global(:last-child) {
-	  margin-bottom: 0;
-	}
- 
-	.info {
-	  --view-bg: hsl(var(--inc));
-	  --view-c: hsl(var(--in));
-	}
- 
-	:global([data-theme='light']) .info {
-	  --view-bg: hsl(var(--in));
-	  --view-c: hsl(var(--inc));
-	}
- 
-	.warning {
-	  --view-bg: hsl(var(--wac));
-	  --view-c: hsl(var(--wa));
-	}
- 
-	:global([data-theme='light']) .warning {
-	  --view-bg: hsl(var(--wa));
-	  --view-c: hsl(var(--wac));
-	}
- </style>
- 
+</style>

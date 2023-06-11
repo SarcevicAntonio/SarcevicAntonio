3974a2371dadd0dabe2ce23d32d274ebcd282192
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-11 17:07:17 +0100

    add self destroying service worker for back compat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
+	import { onMount } from 'svelte'
 	import '../app.postcss'
 	import Footer from './Footer.svelte'
+	import { browser, dev } from '$app/environment'
+
+	onMount(() => {
+		if (!dev && browser && 'serviceWorker' in navigator) {
+			// register self destroying service worker
+			navigator.serviceWorker.register('/sw.js')
+		}
+	})
 </script>
 
 <main>

b3ffe229a306c22fe0ebfe11ada08d8b6a623818
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-01-29 10:33:25 +0100

    remove polyfills and see

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
-import Icons from 'unplugin-icons/vite'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
+import { sveltekit } from '@sveltejs/kit/vite'
+import Icons from 'unplugin-icons/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
-		nodePolyfills(),
 		ViteYaml(),
 		sveltekit(),
 		Icons({

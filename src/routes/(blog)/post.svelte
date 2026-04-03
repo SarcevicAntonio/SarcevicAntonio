9f2bb62ba5cf203eeac2082585c86b48bfc8d4a6
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-04-03 17:31:03 +0200

    remove as-comps dependency

--- src/routes/(blog)/post.svelte
+++ src/routes/(blog)/post.svelte
@@ -5,23 +5,21 @@
 	import '@fontsource/atkinson-hyperlegible/400.css'
 	import '@fontsource/atkinson-hyperlegible/700.css'
 	import Giscus from '@giscus/svelte'
-	import { Notifications, notification } from 'as-comps'
 	import 'prism-themes/themes/prism-a11y-dark.css'
 	import MaterialSymbolsArrowBackRounded from '~icons/material-symbols/arrow-back-rounded'
 
 	let { title, tags, published, updated, summary, children } = $props()
 
+	const copy_icon =
+		'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.413-.588T3 20V7q0-.425.288-.713T4 6q.425 0 .713.288T5 7v13h10q.425 0 .713.288T16 21q0 .425-.288.713T15 22H5Zm4-4q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm0 0V4v12Z"/></svg>'
 	afterNavigate(function add_copy_button_to_codeblocks() {
 		for (const node of document.querySelectorAll('pre > code')) {
 			const button = document.createElement('button')
-			button.innerHTML =
-				'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 22q-.825 0-1.413-.588T3 20V7q0-.425.288-.713T4 6q.425 0 .713.288T5 7v13h10q.425 0 .713.288T16 21q0 .425-.288.713T15 22H5Zm4-4q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm0 0V4v12Z"/></svg>'
-			button.className = 'copy-button'
+			button.innerHTML = copy_icon
+			button.title = 'Copy contents'
 			button.onclick = () => {
 				navigator.clipboard.writeText(node.textContent ?? '')
-				notification('copied content')
 			}
-
 			const wrapper = document.createElement('div')
 			wrapper.className = 'copy-button-wrapper'
 			wrapper.append(button)
@@ -95,8 +93,6 @@
 	</a>
 </section>
 
-<Notifications position="bottom" />
-
 <style>
 	article,
 	section {
@@ -136,53 +132,62 @@
 
 	article {
 		font-family: 'Atkinson Hyperlegible', sans-serif;
+	}
 
-		& :global(ul) {
-			display: flex;
-			flex-direction: column;
-			gap: 0.5rem;
+	article :global(ul) {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	article :global(li > ul) {
+		margin-block-start: 0.5rem;
+	}
+
+	article :global(img) {
+		display: block;
+		max-width: 100%;
+		margin: auto;
+		margin-inline: 0.5rem;
+		border-radius: 0.3em;
+	}
+
+	article :global(p) {
+		line-height: 1.7;
+	}
+
+	article :global(pre) {
+		background-color: black;
+		position: relative;
+		overflow-y: visible;
+		margin-inline: 0.5rem;
+	}
+
+	:global(.copy-button-wrapper) {
+		position: sticky;
+		top: 0;
+		left: 100%;
+		height: 0;
+		width: 0;
+		overflow: visible;
+	}
+
+	:global(.copy-button-wrapper button) {
+		padding: 0;
+		color: var(--as-back-2);
+		line-height: 1;
+		background: none;
+		border: none;
+		opacity: 0.6;
+		position: absolute;
+		top: 0;
+		right: 0;
+		&:hover,
+		&:focus {
+			opacity: 1;
 		}
-
-		& :global(li > ul) {
-			margin-block-start: 0.5rem;
-		}
-
-		& :global(img) {
-			display: block;
-			max-width: 100%;
-			margin: auto;
-			margin-inline: 0.5rem;
-			border-radius: 0.3em;
-		}
-
-		& :global(p) {
-			line-height: 1.7;
-		}
-
-		& :global(pre) {
-			background-color: black;
-			position: relative;
-			overflow-y: visible;
-			margin-inline: 0.5rem;
-			& :global(.copy-button-wrapper) {
-				position: sticky;
-				top: 0;
-				left: 100%;
-				height: 0;
-				width: 0;
-				overflow: visible;
-				& :global(.copy-button) {
-					background: none;
-					border: none;
-					opacity: 0.6;
-					position: absolute;
-					top: 0;
-					right: 0;
-					&:hover {
-						opacity: 1;
-					}
-				}
-			}
+		&:active {
+			color: var(--as-accent);
 		}
 	}
 </style>

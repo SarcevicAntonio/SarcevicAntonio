31eb0b2a95ace3ba98ca618e6232bd76bb708e40
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-04-03 17:43:53 +0200

    fix plugins, update unplugin-icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-import ViteYaml from '@modyfi/vite-plugin-yaml'
 import { sveltekit } from '@sveltejs/kit/vite'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
-		ViteYaml(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte',

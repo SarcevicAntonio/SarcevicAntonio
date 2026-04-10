7693686140d190556f0ae56a6fa28e89e6e9a986
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-04-10 16:16:06 +0200

    add other stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set')
 
 export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
+	schema: './src/lib/server/db/schema',
 	dialect: 'sqlite',
 	dbCredentials: { url: process.env.DATABASE_URL },
 	verbose: true,
-	strict: true
-});
+	strict: true,
+})
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
 	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testMatch: '**/*.e2e.{ts,js}'
-});
+	testMatch: '**/*.e2e.{ts,js}',
+})

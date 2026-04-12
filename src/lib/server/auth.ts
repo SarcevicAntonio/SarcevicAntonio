1707cdb2490223a2b5da0aaa575e70645645a4f4
Antonio Sarcevic <sarcevicantonio@gmail.com>
2026-04-12 17:52:42 +0200

    update zod and fix build?

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,5 +1,5 @@
 import { getRequestEvent } from '$app/server'
-import { BETTER_AUTH_URL } from '$env/static/private'
+import { BETTER_AUTH_SECRET, BETTER_AUTH_URL } from '$env/static/private'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { sveltekitCookies } from 'better-auth/svelte-kit'
@@ -7,6 +7,7 @@ import { db } from './db'
 import * as schema from './db/schema/auth'
 
 export const auth = betterAuth({
+	secret: BETTER_AUTH_SECRET,
 	baseURL: BETTER_AUTH_URL,
 	database: drizzleAdapter(db, { provider: 'sqlite', schema }),
 	plugins: [sveltekitCookies(getRequestEvent)],
--- src/routes/(app)/auth/+page.svelte
+++ src/routes/(app)/auth/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { authClient } from '$lib/auth-client'
+
 	const session = authClient.useSession()
 </script>
 
@@ -18,6 +19,14 @@
 			</button>
 		</div>
 	{:else}
-		login...
+		<form method="POST">
+			<label for="email">email</label>
+			<input type="email" name="email" />
+			<label for="username">username</label>
+			<input type="text" name="username" />
+			<label for="password">password</label>
+			<input type="password" name="password" />
+			<button> register</button>
+		</form>
 	{/if}
 </div>

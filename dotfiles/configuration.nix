d886a3346aba7f657a3e8d751c24ad95bbc97409
Antonio Sarcevic <antonio@sarcevic.dev>
2026-05-09 03:25:35 +0200

    configuration.nix

---
+++ dotfiles/configuration.nix
@@ -0,0 +1,184 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  unstable = import <unstable> {
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
+{
+  # Nix
+  nix.settings.experimental-features = [ "nix-command" ];
+  nixpkgs.config.allowUnfree = true;
+  imports = [ ./hardware-configuration.nix ];
+
+  # Nix OS
+  system.autoUpgrade.enable = true;
+  # determines stateful settings like file locations and database versions
+  # READ BEFORE CHANGING:
+  # https://search.nixos.org/options?channel=25.11&show=system.stateVersion&query=system.stateVersion
+  system.stateVersion = "22.11"; # first install on ex-tides
+
+  # Bootloader
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 3;
+
+  # OS Basics
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  services.printing.enable = true;
+  time.hardwareClockInLocalTime = true; # syncs clock with Windows
+  services.xserver.enable = true;
+  services.xserver.excludePackages = with pkgs; [ xterm ];
+  ## Localization
+  services.localtimed.enable = true;
+  console.keyMap = "de";
+  i18n.defaultLocale = "de_DE.UTF-8";
+  services.xserver.xkb = {
+    layout = "de";
+    variant = "mac_nodeadkeys";
+  };
+  ## Audio
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  security.rtkit.enable = true; # required for PipeWire to acquire realtime priority
+  ## Video
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    open = true; # required for newer GPUs
+    powerManagement.enable = true;
+    nvidiaSettings = false;
+  };
+  boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/var/tmp" ];
+
+  # Desktop Environment
+  ## Gnome
+  services.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.gvfs.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany
+    gnome-music
+    gnome-system-monitor
+    gnome-connections
+    simple-scan
+    gnome-tour
+  # yelp
+  ];
+  ### fix gnomes nautlis file browser media metadata display: https://github.com/NixOS/nixpkgs/issues/53631#issuecomment-3704189416
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+    lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+      [
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
+        pkgs.gst_all_1.gst-libav
+      ];
+
+  # Software
+  services.flatpak.enable = true;
+  programs.appimage = {
+    enable = true;
+    binfmt = true;
+  };
+  environment.systemPackages = with pkgs; [
+    gnome-shell-extensions
+    gnomeExtensions.appindicator # steam and discord force tray icons ;(
+    gnomeExtensions.clipboard-indicator
+    nixfmt
+    vivaldi
+    firefox
+    wget
+    nodejs
+    gh
+    nodePackages.pnpm
+    vscodium
+    flyctl
+    # discord
+    vesktop
+    signal-desktop
+    obsidian
+    micro
+    jellyfin
+    jellyfin-web
+    jellyfin-ffmpeg
+    heroic
+    lutris
+    wineWowPackages.staging
+    unstable.r2modman
+    rpcs3
+    atlauncher # minecraft launcher
+    obs-studio
+    libreoffice
+    hunspell
+    hunspellDicts.de_DE
+    hunspellDicts.en_US
+    handbrake
+    eyedropper # "pastel" is nice too though
+    resources
+    gradia
+    rclone
+	pwvucontrol
+	qbz
+	cartridges
+	eartag
+	tuba
+	ptyxis
+  ];
+  programs.zoxide.enable = true;
+  services.lact.enable = true;
+  hardware.xone.enable = true;
+  programs.git.enable = true;
+  services.jotta-cli.enable = true;
+  virtualisation.waydroid = {
+    enable = true;
+    package = pkgs.waydroid-nftables;
+  };
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "sarcevic";
+  };
+  services.mullvad-vpn = {
+    enable = true;
+    package = pkgs.mullvad-vpn; # also provide GUI tool
+  };
+  programs.gamemode.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+  
+  # User Config
+  users.users.sarcevic = {
+    isNormalUser = true;
+    description = "toni / link";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "sarcevic";
+
+  # Environment
+  environment.sessionVariables.NIXOS_OZONE_WL = "1"; # force Ozone Wayland in Chromium/Electron
+}

ddbc87f6f5a95cfe6a75adfe31933517a5329d2b
Antonio Sarcevic <toni@linkplay.space>
2026-09-23 19:38:15 +0200

    remove mojis

--- src/lib/appearances.ts
+++ src/lib/appearances.ts
@@ -1,9 +1,8 @@
 import type { AppearanceSource } from './server/entries'
 
 export const appearances: AppearanceSource[] = [
-  	{
-		title:
-			'Interview: Bi+ Vorurteile und Stereotype',
+	{
+		title: 'Interview: Bi+ Vorurteile und Stereotype',
 		summary:
 			'IWWIT / Deutsche Aidshilfe interviewed me about biphobia and - erasure as part of their BICONIC action. This interview is in German.',
 		href: 'https://www.iwwit.de/gayzine/bi-vorurteilen-und-stereotypen/',
